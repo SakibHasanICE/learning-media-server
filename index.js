f2a39bcf5d7dfb07833dede0fcafb11ada11dff9
Sakibhasan <hasansakib047@gmai.,com>
2022-10-26 23:22:15 +0600

    course jsn added

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const app = express();
+const cors =require('cors')
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 const courses=require('./data/courses.json');
-app.get('/',(req,res)=>{
-    res.send('news api running ');  
-});
+const course=require('./data/information.json');
 app.get('/courses',(req,res)=>{
     res.send(courses);  
 });
 
+app.get('/course/:id',(req,res)=>{
+    const id=req.params.id;
+    const courseDetails=course.find(e=>e.id===id);
+    res.send(courseDetails);
+});
+
 app.listen(port,()=>{
-    console.log('hsfdkjhsd',port);
+    console.log('working',port);
 })
